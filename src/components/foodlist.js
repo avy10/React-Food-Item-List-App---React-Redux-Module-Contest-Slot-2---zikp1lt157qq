bcd700fa6389d9421d88f676549661030214f09e
Abhishek Kumar <avykr07@gmail.com>
2024-02-12 10:20:24 +0530

    got css from ins

--- src/components/foodlist.js
+++ src/components/foodlist.js
@@ -15,8 +15,11 @@ function FoodList() {
 		setIsFirstCardEnabled(true);
 		setIsSecondCardEnabled(true);
 		setHateU(true);
-		setIsFormEnabled(true);
+		// setIsFormEnabled(true);
 	}
+	const handleFormClick = () => {
+		setIsFormEnabled(true);
+	};
 
 	function meganFox() {
 		const newFood = {
@@ -42,6 +45,91 @@ function FoodList() {
 				<h1>Food Items List</h1>
 				<button onClick={iamPissed}>Add Food</button>
 
+				<div className="card-container">
+					{isFirstCardEnabled ? (
+						<>
+							<h2>Item Name:</h2>
+							<input
+								name="itemName"
+								type="text"
+								disabled={!isFirstCardEnabled}
+								value={itemName}
+								onChange={(e) => setItemName(e.target.value)}
+							/>
+							<h2>Food Type:</h2>
+							<input
+								name="foodType"
+								type="text"
+								disabled={!isFirstCardEnabled}
+								value={foodType}
+								onChange={(e) => setFoodType(e.target.value)}
+							/>
+							<div className={`card`}>
+								<form
+									style={{ opacity: isFormEnabled ? 1 : 0.5 }}
+									onSubmit={(e) => e.preventDefault()}
+									onClick={handleFormClick}
+								>
+									<h2>Spiciness Level:</h2>
+									<input
+										name="spicinessLevel"
+										type="text"
+										disabled={!isFormEnabled}
+										value={spicinessLevel}
+										onChange={(e) =>
+											setSpicinessLevel(e.target.value)
+										}
+									/>
+								</form>
+							</div>
+						</>
+					) : (
+						""
+					)}
+				</div>
+				{isFirstCardEnabled ? (
+					<div
+						className={`card ${
+							isSecondCardEnabled ? "" : "disabled"
+						}`}
+					>
+						<button onClick={meganFox}>Save</button>
+					</div>
+				) : (
+					" "
+				)}
+
+				<ul className="list">
+					{foods.map((ele, index) => (
+						<li key={index}>
+							{ele.name} ({ele.type}) = Spiciness Level:{" "}
+							{ele.hott}
+							<button onClick={() => meguunFaxx(ele.name)}>
+								Delete
+							</button>
+						</li>
+					))}
+					{/* <li>
+						{itemName} ({foodType}) - Spiciness Level:{" "}
+						{spicinessLevel}
+						<button>Delete</button>
+					</li> */}
+				</ul>
+			</div>
+		</>
+	);
+}
+
+export default FoodList;
+
+// original
+/* 
+return (
+		<>
+			<div className="container">
+				<h1>Food Items List</h1>
+				<button onClick={iamPissed}>Add Food</button>
+
 				{hateU && (
 					<>
 						<div className="card-container">
@@ -62,7 +150,11 @@ function FoodList() {
 								onChange={(e) => setFoodType(e.target.value)}
 							/>
 							<div className={`card`}>
-								<form>
+								<form
+									style={{ opacity: isFormEnabled ? 1 : 0.5 }}
+									onSubmit={(e) => e.preventDefault()}
+									onClick={handleFormClick}
+								>
 									<h2>Spiciness Level:</h2>
 									<input
 										name="spicinessLevel"
@@ -96,15 +188,15 @@ function FoodList() {
 							</button>
 						</li>
 					))}
-					{/* <li>
-						{itemName} ({foodType}) - Spiciness Level:{" "}
-						{spicinessLevel}
-						<button>Delete</button>
-					</li> */}
+					// {<li>
+					// 	{itemName} ({foodType}) - Spiciness Level:{" "}
+					// 	{spicinessLevel}
+					// 	<button>Delete</button>
+					// </li> }
 				</ul>
 			</div>
 		</>
 	);
-}
 
-export default FoodList;
+
+*/
